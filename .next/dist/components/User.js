5b36b9a518a028865d76e3d42893196347fdbec8
Aman Rawat <rawataman340@gmail.com>
2020-02-20 12:02:17 +0530

    Initial commit

---
+++ .next/dist/components/User.js
@@ -0,0 +1,46 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _style = require('styled-jsx/style.js');
+
+var _style2 = _interopRequireDefault(_style);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/home/justrawat/Downloads/react-nextjs/components/User.js';
+
+
+var user = function user(props) {
+    return _react2.default.createElement('div', {
+        className: 'jsx-2876080547',
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, _react2.default.createElement('h1', {
+        className: 'jsx-2876080547',
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, props.name), _react2.default.createElement('p', {
+        className: 'jsx-2876080547',
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 6
+        }
+    }, 'Age: ', props.age), _react2.default.createElement(_style2.default, {
+        styleId: '2876080547',
+        css: 'div.jsx-2876080547{border:1px solid #eee;box-shadow:0 2p 3px #ccc;padding:20px;text-align:center;}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvVXNlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFNb0IsQUFHdUMsc0JBQ0cseUJBQ1osYUFDSyxrQkFDdEIiLCJmaWxlIjoiY29tcG9uZW50cy9Vc2VyLmpzIiwic291cmNlUm9vdCI6Ii9ob21lL2p1c3RyYXdhdC9Eb3dubG9hZHMvcmVhY3QtbmV4dGpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuY29uc3QgdXNlciA9IChwcm9wcykgPT4gKFxuICAgIDxkaXY+XG4gICAgICAgIDxoMT57cHJvcHMubmFtZX08L2gxPlxuICAgICAgICA8cD5BZ2U6IHtwcm9wcy5hZ2V9PC9wPlxuICAgICAgICA8c3R5bGUganN4PntgXG4gICAgICAgICAgICBkaXYge1xuICAgICAgICAgICAgICAgIGJvcmRlcjogMXB4IHNvbGlkICNlZWU7XG4gICAgICAgICAgICAgICAgYm94LXNoYWRvdzogMCAycCAzcHggI2NjYztcbiAgICAgICAgICAgICAgICBwYWRkaW5nOiAyMHB4O1xuICAgICAgICAgICAgICAgIHRleHQtYWxpZ246IGNlbnRlcjtcbiAgICAgICAgICAgIH1cbiAgICAgICAgYH08L3N0eWxlPlxuICAgIDwvZGl2PlxuKTtcblxuZXhwb3J0IGRlZmF1bHQgdXNlcjsiXX0= */\n/*@ sourceURL=components/User.js */'
+    }));
+};
+
+exports.default = user;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvVXNlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsInVzZXIiLCJwcm9wcyIsIm5hbWUiLCJhZ2UiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLE9BQU8sU0FBUCxBQUFPLEtBQUEsQUFBQyxPQUFEOzJCQUNULGNBQUE7bUJBQUE7O3NCQUFBO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ksY0FBQTttQkFBQTs7c0JBQUE7d0JBQUEsQUFBSztBQUFMO0FBQUEsYUFESixBQUNJLEFBQVcsQUFDWCx1QkFBQSxjQUFBO21CQUFBOztzQkFBQTt3QkFBQTtBQUFBO0FBQUEsT0FBUyxlQUZiLEFBRUksQUFBZTtpQkFGbkI7YUFEUyxBQUNUO0FBQUE7QUFESixBQWVBOztrQkFBQSxBQUFlIiwiZmlsZSI6IlVzZXIuanMiLCJzb3VyY2VSb290IjoiL2hvbWUvanVzdHJhd2F0L0Rvd25sb2Fkcy9yZWFjdC1uZXh0anMifQ==
